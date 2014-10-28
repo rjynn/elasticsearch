923cd690f43b4c7200bf3765d85490ada7a2ca4e
Victor Guana <guana@ualberta.ca>
2014-10-28 15:07:44 -0600

    First commit

---
+++ AndroidElasticSearch/src/ca/ualberta/ssrg/movies/MainActivity.java
@@ -0,0 +1,175 @@
+package ca.ualberta.ssrg.movies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+import ca.ualberta.ssrg.androidelasticsearch.R;
+import ca.ualberta.ssrg.movies.es.ESMovieManager;
+import ca.ualberta.ssrg.movies.es.IMovieManager;
+import ca.ualberta.ssrg.movies.es.Movie;
+
+public class MainActivity extends Activity {
+
+	private ListView movieList;
+	private List<Movie> movies;
+	private ArrayAdapter<Movie> moviesViewAdapter;
+
+	private IMovieManager movieManager;
+
+	private Context mContext = this;
+
+	// Thread to update adapter after an operation
+	private Runnable doUpdateGUIList = new Runnable() {
+		public void run() {
+			moviesViewAdapter.notifyDataSetChanged();
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		movieList = (ListView) findViewById(R.id.movieList);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+		movies = new ArrayList<Movie>();
+		moviesViewAdapter = new ArrayAdapter<Movie>(this, R.layout.list_item,movies);
+		movieList.setAdapter(moviesViewAdapter);
+		movieManager = new ESMovieManager();
+
+		// Show details when click on a movie
+		movieList.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int pos,	long id) {
+				int movieId = movies.get(pos).getId();
+				startDetailsActivity(movieId);
+			}
+
+		});
+
+		// Delete movie on long click
+		movieList.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+				Movie movie = movies.get(position);
+				Toast.makeText(mContext, "Deleting " + movie.getTitle(), Toast.LENGTH_LONG).show();
+
+				Thread thread = new DeleteThread(movie.getId());
+				thread.start();
+
+				return true;
+			}
+		});
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		// Refresh the list when visible
+		// TODO: Search all
+		movies.clear();
+		Thread thread = new SearchThread("");
+		thread.start();
+	}
+
+	/** 
+	 * Search for movies with a given word(s) in the text view
+	 * @param view
+	 */
+	public void search(View view) {
+		movies.clear();
+
+		// TODO: Extract search query from text view
+		EditText tv = (EditText) findViewById(R.id.editText1);
+		String searchText = tv.getText().toString();
+
+		// TODO: Run the search thread
+		movies.clear();
+		Thread thread = new SearchThread(searchText);
+		thread.start();
+	}
+	
+	/**
+	 * Starts activity with details for a movie
+	 * @param movieId Movie id
+	 */
+	public void startDetailsActivity(int movieId) {
+		Intent intent = new Intent(mContext, DetailsActivity.class);
+		intent.putExtra(DetailsActivity.MOVIE_ID, movieId);
+
+		startActivity(intent);
+	}
+	
+	/**
+	 * Starts activity to add a new movie
+	 * @param view
+	 */
+	public void add(View view) {
+		Intent intent = new Intent(mContext, AddActivity.class);
+		startActivity(intent);
+	}
+
+
+	class SearchThread extends Thread {
+		// TODO: Implement search thread
+		private String search;
+		
+		public SearchThread(String s) {
+			search = s;
+		}
+
+		@Override
+		public void run() {
+			movies.clear();
+			movies.addAll(movieManager.searchMovies(search, null));
+			
+			runOnUiThread(doUpdateGUIList);
+		}
+	}
+
+	
+	class DeleteThread extends Thread {
+		private int movieId;
+
+		public DeleteThread(int movieId) {
+			this.movieId = movieId;
+		}
+
+		@Override
+		public void run() {
+			movieManager.deleteMovie(movieId);
+
+			// Remove movie from local list
+			for (int i = 0; i < movies.size(); i++) {
+				Movie m = movies.get(i);
+
+				if (m.getId() == movieId) {
+					movies.remove(m);
+					break;
+				}
+			}
+
+			runOnUiThread(doUpdateGUIList);
+		}
+	}
+}
---
+++ AndroidElasticSearch/src/ca/ualberta/ssrg/movies/es/ESMovieManager.java
@@ -0,0 +1,218 @@
+package ca.ualberta.ssrg.movies.es;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Log;
+
+import ca.ualberta.ssrg.movies.es.data.Hits;
+import ca.ualberta.ssrg.movies.es.data.SearchResponse;
+import ca.ualberta.ssrg.movies.es.data.SearchHit;
+import ca.ualberta.ssrg.movies.es.data.SimpleSearchCommand;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class ESMovieManager implements IMovieManager {
+
+	private static final String SEARCH_URL = "http://cmput301.softwareprocess.es:8080/testing/movie/_search";
+	private static final String RESOURCE_URL = "http://cmput301.softwareprocess.es:8080/testing/movie/";
+	private static final String TAG = "MovieSearch";
+
+	private Gson gson;
+
+	public ESMovieManager() {
+		gson = new Gson();
+	}
+
+	/**
+	 * Get a movie with the specified id
+	 */
+	public Movie getMovie(int id) {
+
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(RESOURCE_URL + id);
+
+		HttpResponse response;
+
+		try {
+			response = httpClient.execute(httpGet);
+			SearchHit<Movie> sr = parseMovieHit(response);
+			return sr.getSource();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+
+		return null;
+	}
+
+	
+
+	/**
+	 * Get movies with the specified search string. If the search does not
+	 * specify fields, it searches on all the fields.
+	 */
+	public List<Movie> searchMovies(String searchString, String field) {
+		List<Movie> result = new ArrayList<Movie>();
+
+		// TODO: Implement search movies using ElasticSearch
+		if (searchString == null || "".equals(searchString)) {
+			searchString = "*";
+		}
+		
+		HttpClient httpClient = new DefaultHttpClient();
+		
+		try {
+			HttpPost searchRequest = createSearchRequest(searchString, field);
+			
+			HttpResponse response = httpClient.execute(searchRequest);
+			
+			String status = response.getStatusLine().toString();
+			Log.i(TAG, status);
+			
+			SearchResponse<Movie> esResponse = parseSearchResponse(response);
+			Hits<Movie> hits = esResponse.getHits();
+			
+			if (hits != null) {
+				if (hits.getHits() != null) {
+					for (SearchHit<Movie> sesr : hits.getHits()) {
+						result.add(sesr.getSource());
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+
+		return result;
+	}
+
+	/**
+	 * Adds a new movie
+	 */
+	public void addMovie(Movie movie) {
+		HttpClient httpClient = new DefaultHttpClient();
+
+		try {
+			HttpPost addRequest = new HttpPost(RESOURCE_URL + movie.getId());
+
+			StringEntity stringEntity = new StringEntity(gson.toJson(movie));
+			addRequest.setEntity(stringEntity);
+			addRequest.setHeader("Accept", "application/json");
+
+			HttpResponse response = httpClient.execute(addRequest);
+			String status = response.getStatusLine().toString();
+			Log.i(TAG, status);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Deletes the movie with the specified id
+	 */
+	public void deleteMovie(int movieId) {
+		HttpClient httpClient = new DefaultHttpClient();
+
+		try {
+			HttpDelete deleteRequest = new HttpDelete(RESOURCE_URL + movieId);
+			deleteRequest.setHeader("Accept", "application/json");
+
+			HttpResponse response = httpClient.execute(deleteRequest);
+			String status = response.getStatusLine().toString();
+			Log.i(TAG, status);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Creates a search request from a search string and a field
+	 */
+	private HttpPost createSearchRequest(String searchString, String field)	throws UnsupportedEncodingException {
+		
+		HttpPost searchRequest = new HttpPost(SEARCH_URL);
+
+		String[] fields = null;
+		if (field != null) {
+			fields = new String[1];
+			fields[0] = field;
+		}
+		
+		SimpleSearchCommand command = new SimpleSearchCommand(searchString,	fields);
+		
+		String query = command.getJsonCommand();
+		Log.i(TAG, "Json command: " + query);
+
+		StringEntity stringEntity;
+		stringEntity = new StringEntity(query);
+
+		searchRequest.setHeader("Accept", "application/json");
+		searchRequest.setEntity(stringEntity);
+
+		return searchRequest;
+	}
+	
+	private SearchHit<Movie> parseMovieHit(HttpResponse response) {
+		
+		try {
+			String json = getEntityContent(response);
+			Type searchHitType = new TypeToken<SearchHit<Movie>>() {}.getType();
+			
+			SearchHit<Movie> sr = gson.fromJson(json, searchHitType);
+			return sr;
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	/**
+	 * Parses the response of a search
+	 */
+	private SearchResponse<Movie> parseSearchResponse(HttpResponse response) throws IOException {
+		String json;
+		json = getEntityContent(response);
+
+		Type searchResponseType = new TypeToken<SearchResponse<Movie>>() {
+		}.getType();
+		
+		SearchResponse<Movie> esResponse = gson.fromJson(json, searchResponseType);
+
+		return esResponse;
+	}
+
+	/**
+	 * Gets content from an HTTP response
+	 */
+	public String getEntityContent(HttpResponse response) throws IOException {
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+
+		return result.toString();
+	}
+}
